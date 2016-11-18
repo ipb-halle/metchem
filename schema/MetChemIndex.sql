791fa06f7bf5eb433d738f6224ca19087917e908
Christoph Ruttkies <chrisr@troll>
2016-11-18 17:22:52 +0100

    adaptec schema and index

--- schema/MetChemIndex.sql
+++ schema/MetChemIndex.sql
@@ -1,6 +1,6 @@
 -- create index compound_structure on compound  using gist (mol_structure);
--- create index compound_mass on compound (exact_mass);
-create index compound_formula on compound (formula);                        
+create index compound_mass on compound (monoisotopic_mass);
+create index compound_formula on compound (molecular_formula);                        
 create index compound_inchikey1 on compound (inchi_key_1);                        
 
 create index substance_compound on substance (compound_id);
